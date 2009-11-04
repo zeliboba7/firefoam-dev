16e8b3bdd8865dea651f4a950c2ebae59577b3c9
fmglobal.oss@gmail.com <fmglobal.oss@gmail.com@e474090e-a86b-11de-ac05-951101a8bf3e>
2009-11-04 15:21:36 +0000

    Change comment lines only

--- trunk/fireFoam-1.6-1.0/source/diffusionCombustionThermophysicalModels/fireMixture/fireMixture.C
+++ trunk/fireFoam-1.6-1.0/source/diffusionCombustionThermophysicalModels/fireMixture/fireMixture.C
@@ -1,36 +1,27 @@
 /*---------------------------------------------------------------------------*\
-  =========                 |
-  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
-   \\    /   O peration     |
-    \\  /    A nd           | Copyright held by original developer
-     \\/     M anipulation  |
--------------------------------------------------------------------------------
-    This file is part of a preliminary version of the FireFOAM code which is 
-    currently under development at FM Global. FM Global designates this file
-    WITHOUT ANY WARRANTY to be used for development purpose only. 
 
-License
-    This file is based on OpenFOAM.
-
-    OpenFOAM is free software; you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by the
-    Free Software Foundation; either version 2 of the License, or (at your
-    option) any later version.
-
-    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
-    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-    for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with OpenFOAM; if not, write to the Free Software Foundation,
-    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-
-Class
-    fireMixture
-
-Description
-
+     This file is part of a preliminary version of the FireFOAM, LES solver for 
+     fire applications, which is currently under development at FM Global and 
+     OpenCFD. This file is based on OpenFOAM, the Open Source CFD Toolbox.
+     
+     Copyright (c) 2009 OpenCFD / FM Global        
+  
+ License 
+       
+     FireFOAM is free software; you can redistribute it and/or modify it 
+     under the terms of the GNU General Public License as published by the 
+     Free Software Foundation; either version 2 of the License, or (at your 
+     option) any later version. 
+  
+     FireFOAM is distributed in the hope that it will be useful, but WITHOUT 
+     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+     FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+     for more details. 
+  
+     You should have received a copy of the GNU General Public License 
+     along with FireFOAM; if not, write to the Free Software Foundation, 
+     Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA 
+  
 \*---------------------------------------------------------------------------*/
 
 #include "fireMixture.H"
--- trunk/fireFoam-1.6-1.0/source/diffusionCombustionThermophysicalModels/interpolationLookUpTable/interpolationLookUpTable.C
+++ trunk/fireFoam-1.6-1.0/source/diffusionCombustionThermophysicalModels/interpolationLookUpTable/interpolationLookUpTable.C
@@ -1,27 +1,27 @@
 /*---------------------------------------------------------------------------*\
-  =========                 |
-  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
-   \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 1991-2008 OpenCFD Ltd.
-     \\/     M anipulation  |
--------------------------------------------------------------------------------
-License
-    This file is part of OpenFOAM.
 
-    OpenFOAM is free software; you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by the
-    Free Software Foundation; either version 2 of the License, or (at your
-    option) any later version.
-
-    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
-    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-    for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with OpenFOAM; if not, write to the Free Software Foundation,
-    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-
+     This file is part of a preliminary version of the FireFOAM, LES solver for 
+     fire applications, which is currently under development at FM Global and 
+     OpenCFD. This file is based on OpenFOAM, the Open Source CFD Toolbox.
+     
+     Copyright (c) 2009 OpenCFD / FM Global        
+  
+ License 
+       
+     FireFOAM is free software; you can redistribute it and/or modify it 
+     under the terms of the GNU General Public License as published by the 
+     Free Software Foundation; either version 2 of the License, or (at your 
+     option) any later version. 
+  
+     FireFOAM is distributed in the hope that it will be useful, but WITHOUT 
+     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+     FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+     for more details. 
+  
+     You should have received a copy of the GNU General Public License 
+     along with FireFOAM; if not, write to the Free Software Foundation, 
+     Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA 
+  
 \*---------------------------------------------------------------------------*/
 
 #include "IFstream.H"
